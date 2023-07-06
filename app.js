43cd71f0b9ccf88de1f6f72e907f52b01a4949fe
jeffrey suarez <79020208+JeffreySuarez@users.noreply.github.com>
2023-07-06 18:51:12 -0500

    ajustando filtro de usuarios

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+let data = [
+  {
+    name: "pedro",
+    password: "1234",
+  },
+  {
+    name: "paola",
+    password: "5678",
+  },
+  {
+    name: "clara",
+    password: "9101",
+  },
+];
+
 function login(form) {
-  if (form.usuario.value === "pedro") {
-    if (form.password.value === "1234") {
+  let nombres = data.map((e) => e.name);
+  console.log(nombres);
+
+  let filtrarUsuario = nombres.find((e) => e === form.usuario.value);
+  console.log(filtrarUsuario);
+
+  let password = data.map((e) => e.password);
+  console.log(password);
+
+  let filtrarPassword = password.find((e) => e === form.password.value);
+  console.log(filtrarPassword);
+
+  if (form.usuario.value === filtrarUsuario) {
+    if (form.password.value === filtrarPassword) {
       location = "home.html";
     } else {
       console.log("error de password");
