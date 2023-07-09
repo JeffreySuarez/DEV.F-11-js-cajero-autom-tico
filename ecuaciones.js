20047d7e6b8294a2b38eae37a413f2841d111a0b
jeffrey suarez <79020208+JeffreySuarez@users.noreply.github.com>
2023-07-08 20:20:06 -0500

    ajustes error de valores al ingresar

--- ecuaciones.js
+++ ecuaciones.js
@@ -1,11 +1,19 @@
+let lecturaCantidad = parseInt(document.getElementById("cantidad").value);
+let lecturaSaldoActual = parseInt(document.getElementById("cuenta").value);
+
 function ecuacion1() {
   let cantidadTotal = parseInt(document.getElementById("cantidad").value);
   //esta constante va a tomar y transformar a numero que agregarmos en el input de cantidad
   let cantidadCuenta = parseInt(document.getElementById("cuenta").value);
   //obtenemos los datos de cuenta
-
+  const error = document.getElementById("error");
   //vamos a realizar los calculos
-  document.getElementById("cuenta").value = cantidadCuenta + cantidadTotal;
+
+  if (cantidadCuenta > 990) {
+    error.innerHTML = "Error el valor no puede ser mayor que 990";
+  } else {
+    document.getElementById("cuenta").value = cantidadCuenta + cantidadTotal;
+  }
 }
 
 function ecuacion2() {
@@ -13,8 +21,12 @@ function ecuacion2() {
   //esta constante va a tomar y transformar a numero que agregarmos en el input de cantidad
   let cantidadCuenta = parseInt(document.getElementById("cuenta").value);
   //obtenemos los datos de cuenta
+  const error = document.getElementById("error");
 
   //vamos a realizar los calculos
-
-  document.getElementById("cuenta").value = cantidadCuenta - cantidadTotal;
+  if (cantidadCuenta < 10) {
+    error.innerHTML = "Error el valor no puede ser menor que 10 o mayor que 990";
+  } else {
+    document.getElementById("cuenta").value = cantidadCuenta - cantidadTotal;
+  }
 }
